f1172474820a277e09189192c63a72aeb3bd2232
Rafal Kluszczynski <klusi@icm.edu.pl>
2014-04-10 12:32:10 +0200

    tested im4java operations and prepared sample task for further implementation

---
+++ compare-engine/src/main/java/pl/info/rkluszczynski/image/engine/tasks/Im4JavaTask.java
@@ -0,0 +1,42 @@
+package pl.info.rkluszczynski.image.engine.tasks;
+
+import org.im4java.core.ConvertCmd;
+import org.im4java.core.IMOperation;
+import org.im4java.core.Stream2BufferedImage;
+import pl.info.rkluszczynski.image.engine.model.SessionData;
+
+import java.awt.image.BufferedImage;
+import java.math.BigDecimal;
+
+import static pl.info.rkluszczynski.image.engine.model.ImageStatisticNames.DUMMY_RESULT;
+
+
+public class Im4JavaTask extends AbstractTask {
+    public Im4JavaTask(SessionData sessionData) {
+        super(sessionData);
+    }
+
+    @Override
+    public void processImageData(BufferedImage inputImage, BufferedImage templateImage) {
+        IMOperation op = new IMOperation();
+        op.addImage();                        // input
+        op.blur(2.0).paint(10.0);
+        op.addImage("png:-");                 // output: stdout
+
+        // Set up command:
+        ConvertCmd convert = new ConvertCmd();
+        Stream2BufferedImage s2b = new Stream2BufferedImage();
+        convert.setOutputConsumer(s2b);
+
+        // Run command and extract BufferedImage from OutputConsumer:
+        try {
+            convert.run(op, inputImage);
+        } catch (Exception e) {
+            logger.error("Unable to run convert operation!", e);
+        }
+        BufferedImage resultImage = s2b.getImage();
+
+        saveResultImage(resultImage);
+        saveStatisticData(DUMMY_RESULT, BigDecimal.ZERO);
+    }
+}
--- testing-im4java/src/main/java/pl/info/rkluszczynski/test/im4java/TestIm4JavaApp.java
+++ testing-im4java/src/main/java/pl/info/rkluszczynski/test/im4java/TestIm4JavaApp.java
@@ -7,7 +7,9 @@ import org.im4java.core.IMOperation;
 import org.im4java.core.Stream2BufferedImage;
 import org.im4java.process.ProcessStarter;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 public class TestIm4JavaApp {
@@ -15,13 +17,15 @@ public class TestIm4JavaApp {
     private static String RESOURCE_RELATIVE_PATH = "testing-im4java/src/main/resources/";
     private static String GLOBAL_SEARCH_PATH = "D:\\PortableApps\\ImageMagick-6.8.8-7";
 
-    public static void main(String[] args) throws InterruptedException, IOException, IM4JavaException {
+    public static void main(String[] args) throws Exception {
         System.out.println(System.getProperty("user.dir"));
         // Overwriting IM4JAVA_TOOLPATH:
         ProcessStarter.setGlobalSearchPath(GLOBAL_SEARCH_PATH);
 
-        runResizeFromFileToFile();
-        readBufferedImageFromFile();
+//        runResizeFromFileToFile();
+//        readConvertedBufferedImageFromFile();
+
+        convertBufferedImageToBufferedImage();
     }
 
 
@@ -31,7 +35,7 @@ public class TestIm4JavaApp {
 
         // Create the operation, add images and operators/options:
         IMOperation op = new IMOperation();
-        op.addImage(RESOURCE_RELATIVE_PATH + "image.jpg");
+        op.addImage(RESOURCE_RELATIVE_PATH + "image.png");
         op.resize(800, 600);
         op.addImage(RESOURCE_RELATIVE_PATH + "tmp-result-image.jpg");
 
@@ -39,12 +43,12 @@ public class TestIm4JavaApp {
         cmd.run(op);
     }
 
-    private static void readBufferedImageFromFile() throws InterruptedException, IOException, IM4JavaException {
+    private static void readConvertedBufferedImageFromFile() throws InterruptedException, IOException, IM4JavaException {
         IMOperation op = new IMOperation();
         op.addImage();                        // input
         op.addImage("png:-");                 // output: stdout
         String[] images = new String[] {
-                RESOURCE_RELATIVE_PATH + "image.jpg"
+                RESOURCE_RELATIVE_PATH + "image.png"
         };
 
         // Set up command:
@@ -53,10 +57,35 @@ public class TestIm4JavaApp {
         convert.setOutputConsumer(s2b);
 
         // Run command and extract BufferedImage from OutputConsumer:
-        convert.run(op,(Object[]) images);
+        convert.run(op, (Object[]) images);
         BufferedImage img = s2b.getImage();
 
         // Print some info on console:
         System.out.println(img.toString());
+    }
+
+    private static void convertBufferedImageToBufferedImage() throws Exception {
+        BufferedImage inputImage = ImageIO.read(
+                new File(RESOURCE_RELATIVE_PATH + "image.png")
+        );
+
+        IMOperation op = new IMOperation();
+        op.addImage();                        // input
+        op.blur(2.0).paint(10.0);
+        op.addImage("png:-");                 // output: stdout
+
+        // Set up command:
+        ConvertCmd convert = new ConvertCmd();
+        Stream2BufferedImage s2b = new Stream2BufferedImage();
+        convert.setOutputConsumer(s2b);
+
+        // Run command and extract BufferedImage from OutputConsumer:
+        convert.run(op, inputImage);
+        BufferedImage resultImage = s2b.getImage();
+
+        System.out.println(resultImage.toString());
+        ImageIO.write(resultImage, "PNG",
+                new File(RESOURCE_RELATIVE_PATH + "tmp-result-blurred.png")
+        );
     }
 }
