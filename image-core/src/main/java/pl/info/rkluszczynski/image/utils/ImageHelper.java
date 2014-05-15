32b1b1ec4f84ae068a27308a7ba8778fd69d2173
Rafal Kluszczynski <klusi@icm.edu.pl>
2014-05-15 13:24:57 +0200

    pixel value scale class

--- image-core/src/main/java/pl/info/rkluszczynski/image/utils/ImageHelper.java
+++ image-core/src/main/java/pl/info/rkluszczynski/image/utils/ImageHelper.java
@@ -1,5 +1,6 @@
 package pl.info.rkluszczynski.image.utils;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.WritableRaster;
@@ -16,6 +17,24 @@ public class ImageHelper {
         boolean isAlphaPremultiplied = colorModel.isAlphaPremultiplied();
         WritableRaster raster = bufferedImage.copyData(null);
         return new BufferedImage(colorModel, raster, isAlphaPremultiplied, null);
+    }
+
+    public static BufferedImage scaleImagePixelsValue(BufferedImage bufferedImage, double scaleRatio) {
+        BufferedImage image = deepCopy(bufferedImage);
+        for (int iw = 0; iw < image.getWidth(); ++iw) {
+            for (int ih = 0; ih < image.getHeight(); ++ih) {
+                Color imagePixelColor = new Color(bufferedImage.getRGB(iw, ih));
+
+                Color newPixelColor = new Color(
+                        (int) Math.min(Math.max(scaleRatio * imagePixelColor.getRed(), 0.), 255.),
+                        (int) Math.min(Math.max(scaleRatio * imagePixelColor.getGreen(), 0.), 255.),
+                        (int) Math.min(Math.max(scaleRatio * imagePixelColor.getBlue(), 0.), 255.),
+                        imagePixelColor.getAlpha()
+                );
+                image.setRGB(iw, ih, newPixelColor.getRGB());
+            }
+        }
+        return image;
     }
 
 }
