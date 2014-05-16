a6e0ce7e712ef1180e16e48286299ea6f8ec4930
Rafal Kluszczynski <rkluszczynski@gmail.com>
2014-05-17 01:09:32 +0200

    naming statistics

--- compare-engine/src/main/java/pl/info/rkluszczynski/image/engine/model/ImageStatisticNames.java
+++ compare-engine/src/main/java/pl/info/rkluszczynski/image/engine/model/ImageStatisticNames.java
@@ -2,6 +2,11 @@ package pl.info.rkluszczynski.image.engine.model;
 
 public enum ImageStatisticNames {
 
+    METRIC_VALUE_ABS("Metric ABS value"),
+    METRIC_VALUE_RMSE("Metric RMSE value"),
+    METRIC_VALUE_NRMSE("Metric NRMSE value"),
+    METRIC_VALUE_PSNR("Metric PSNR value"),
+
     DIFFERENCE_COEFFICIENT("Difference coefficient value"),
 
     DUMMY_RESULT("Dummy result value"),
