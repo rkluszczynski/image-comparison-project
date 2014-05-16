74fc9e246107d1c90992a4d94102b421aa7ba759
Rafal Kluszczynski <rkluszczynski@gmail.com>
2014-05-17 00:56:22 +0200

    again fixing psnr metric

--- compare-engine/src/main/java/pl/info/rkluszczynski/image/engine/tasks/metric/PSNRMetric.java
+++ compare-engine/src/main/java/pl/info/rkluszczynski/image/engine/tasks/metric/PSNRMetric.java
@@ -5,12 +5,12 @@ import java.awt.*;
 public class PSNRMetric implements Metric {
 
     private double mseMetricValue = 0.;
-    private double pixelMaxValue = 0.;
+    private double pixelMaxValue = 255.;
     private double pixelsNumber = 0.;
 
     @Override
     public double calculateValue() {
-        return 20. * Math.log10(pixelMaxValue) - 10. * Math.log10(mseMetricValue / pixelsNumber);
+        return 20. * Math.log10(pixelMaxValue) - 10. * Math.log10(mseMetricValue / (3. * pixelsNumber));
     }
 
     @Override
@@ -19,13 +19,5 @@ public class PSNRMetric implements Metric {
         mseMetricValue += ((inputPixel.getGreen() - templatePixel.getGreen()) * (inputPixel.getGreen() - templatePixel.getGreen()));
         mseMetricValue += ((inputPixel.getBlue() - templatePixel.getBlue()) * (inputPixel.getBlue() - templatePixel.getBlue()));
         ++pixelsNumber;
-
-        pixelMaxValue = Math.max(pixelMaxValue, inputPixel.getRed());
-        pixelMaxValue = Math.max(pixelMaxValue, inputPixel.getGreen());
-        pixelMaxValue = Math.max(pixelMaxValue, inputPixel.getBlue());
-
-        pixelMaxValue = Math.max(pixelMaxValue, templatePixel.getRed());
-        pixelMaxValue = Math.max(pixelMaxValue, templatePixel.getGreen());
-        pixelMaxValue = Math.max(pixelMaxValue, templatePixel.getBlue());
     }
 }
