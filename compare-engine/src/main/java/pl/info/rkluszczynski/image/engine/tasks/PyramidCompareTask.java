c4b81641637c41051ee1cba12189aff12ac3c5ad
Rafal Kluszczynski <rkluszczynski@gmail.com>
2014-05-25 20:01:23 +0200

    logging information about alpha pixels

--- compare-engine/src/main/java/pl/info/rkluszczynski/image/engine/tasks/PyramidCompareTask.java
+++ compare-engine/src/main/java/pl/info/rkluszczynski/image/engine/tasks/PyramidCompareTask.java
@@ -34,6 +34,9 @@ public class PyramidCompareTask extends AbstractTask {
         BufferedImage resultImage = ImageHelper.scaleImagePixelsValue(
                 ImageSizeScaleProcessor.getExactScaledImage(inputImage, compromiseWidth, compromiseHeight), 0.8);
 
+        BufferedImageWrapper templateImageWrapper = new BufferedImageWrapper(templateImage);
+        logger.info("Number of non alpha pixels: {} (out of {})", templateImageWrapper.countNonAlphaPixels(),
+                templateImage.getWidth() * templateImage.getHeight());
         for (int scaleStep = -SCALE_PYRAMID_DEPTH; scaleStep <= SCALE_PYRAMID_DEPTH; ++scaleStep) {
             double scaleFactor = 1. + scaleStep * SCALE_PYRAMID_RATIO;
 
@@ -46,7 +49,7 @@ public class PyramidCompareTask extends AbstractTask {
                     ImageSizeScaleProcessor.getExactScaledImage(inputImage, pyramidStepDesiredWidth, pyramidStepDesiredHeight);
 
             SingleScaleStepProcessor singleScaleStepProcessor = SingleScaleStepProcessor.create(
-                    scaledInputImage, new BufferedImageWrapper(templateImage), metric, scaleFactor
+                    scaledInputImage, templateImageWrapper, metric, scaleFactor
             );
             singleScaleStepProcessor.process(resultImage, this, FULL_SCALE_STEP_PROGRESS);
         }
