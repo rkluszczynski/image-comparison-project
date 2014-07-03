e790d1e18b02dd4e94e95f03ff62b15f540c41c0
Rafal Kluszczynski <klusi@icm.edu.pl>
2014-07-03 10:21:03 +0200

    working with backbone configuration

---
+++ testing-backbone-js/src/main/java/pl/info/rkluszczynski/backbone/config/BackboneMvcConfig.java
@@ -0,0 +1,16 @@
+package pl.info.rkluszczynski.backbone.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+public class BackboneMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+}
