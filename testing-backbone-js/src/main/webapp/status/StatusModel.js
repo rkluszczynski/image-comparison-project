a7fcaa6de155a3a6d789991f3e32414f789f7bf8
Rafal Kluszczynski <rkluszczynski@gmail.com>
2014-07-04 00:38:03 +0200

    working on backbone example

---
+++ testing-backbone-js/src/main/webapp/status/StatusModel.js
@@ -0,0 +1,10 @@
+define(function(require) {
+	var Backbone = require('Backbone');
+
+	return Backbone.Model.extend({
+		urlRoot: 'http://localhost:8080/webapp/processing',
+		url: function() {
+			return this.urlRoot + '?name=' + this.id;
+		}
+	});
+});
