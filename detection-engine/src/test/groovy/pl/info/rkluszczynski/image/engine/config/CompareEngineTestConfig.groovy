7e6e6628851cfd0da42e5e1d33c91bb2b1903472
Rafal Kluszczynski <rkluszczynski@gmail.com>
2014-07-04 13:54:14 +0200

    fixes after renaming

--- detection-engine/src/test/groovy/pl/info/rkluszczynski/image/engine/config/CompareEngineTestConfig.groovy
+++ detection-engine/src/test/groovy/pl/info/rkluszczynski/image/engine/config/CompareEngineTestConfig.groovy
@@ -4,7 +4,7 @@ package pl.info.rkluszczynski.image.engine.config
  * Created by Rafal on 2014-06-19.
  */
 class CompareEngineTestConfig {
-    static String SUB_PROJECT_NAME = 'compare-engine'
+    static String SUB_PROJECT_NAME = 'detection-engine'
 
     public static String getTestResourcesDirectory() {
         String cwd = System.getProperty('user.dir')
